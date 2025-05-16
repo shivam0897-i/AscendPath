d5aba4fa76103a12464986189e3bf2e26ec13b89
shivamagarwal2211 <shivamagarwal2211@gmail.com>
2025-05-16 12:35:03 +0000

    heroimage

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -52,7 +52,7 @@ const HeroImage: React.FC<HeroImageProps> = ({ onClick }) => {
       {/* Main character */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <img 
-          src="https://i.pravatar.cc/300?img=25" 
+          src="/images/logo.jpg" 
           alt="Student" 
           className="w-40 h-40 md:w-48 md:h-48 rounded-full border-4 border-white shadow-lg object-cover floating" 
           onContextMenu={(e) => e.preventDefault()} // Disable right-click
